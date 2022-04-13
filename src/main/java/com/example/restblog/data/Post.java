7d22121758c8b21fc247e5c3f082e71394aea439
Justin Sixsmith <justinsixsmith@gmail.com>
2022-04-13 11:00:20 -0500

    adds hello controller and post class

---
+++ src/main/java/com/example/restblog/data/Post.java
@@ -0,0 +1,15 @@
+package com.example.restblog.data;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+
+}
